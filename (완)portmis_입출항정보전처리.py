9c2a8dd68db44572fe236fc4faebbf72bf0cce27
vanessajchoi <vanessajchoi@gmail.com>
2023-01-20 18:58:31 +0900

    [230120] REFACTOR port_mis + mmsi 병합 및 전처리 완료

--- (완)portmis_입출항정보전처리.py
+++ (완)portmis_입출항정보전처리.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[100]:
+# In[534]:
 
 
 import csv
@@ -9,13 +9,13 @@ import pandas as pd
 import numpy as np
 
 
-# In[437]:
+# In[535]:
 
 
 df_portmis = pd.read_csv("C:/Users/user/Desktop/항만운영정보시스템data/port-mis선박입출항조회.csv", encoding = 'cp949')
 
 
-# In[438]:
+# In[536]:
 
 
 df_portmis
@@ -23,25 +23,25 @@ df_portmis
 
 # ### 선박용도 필터: 국제카페리, 여객선, 기타선 삭제
 
-# In[439]:
+# In[537]:
 
 
 df_portmis['선박용도'].unique()
 
 
-# In[440]:
+# In[538]:
 
 
 len(df_portmis['선박용도'].unique())
 
 
-# In[441]:
+# In[539]:
 
 
 df_portmis.drop(df_portmis[(df_portmis['선박용도'] == '여객선') | (df_portmis['선박용도'] == '국제카페리') | (df_portmis['선박용도'] == '기타선') ].index, inplace=True)
 
 
-# In[442]:
+# In[540]:
 
 
 df_portmis['선박용도'].unique()
@@ -49,20 +49,20 @@ df_portmis['선박용도'].unique()
 
 # ### 입출항 필터: 입항/출항 中 출항 삭제
 
-# In[443]:
+# In[541]:
 
 
 df_portmis['입출'].unique()
 
 
-# In[444]:
+# In[542]:
 
 
 df_portmis = df_portmis.drop(index = df_portmis[df_portmis['입출'] == '출항'].index)
 df_portmis
 
 
-# In[445]:
+# In[543]:
 
 
 df_portmis['입출'].unique()
@@ -70,76 +70,105 @@ df_portmis['입출'].unique()
 
 # ### 구분 필터: 최종/최초 中 최초 삭제 
 
-# In[446]:
+# In[544]:
 
 
 df_portmis = df_portmis.drop(index = df_portmis[df_portmis['구분'] == '최초'].index)
 df_portmis.reset_index(drop=True, inplace = True)
 
 
-# ### 열 삭제: 입항횟수, 입항횟수.1 삭제
-
-# In[447]:
-
-
-df_portmis = df_portmis.drop(df_portmis.iloc[:,3:5], axis = 1)
-
-
-# In[448]:
+# In[545]:
 
 
 df_portmis
 
 
-# In[449]:
+# ### 열 삭제: 입항횟수, 입항횟수.1 삭제
+
+# In[546]:
 
 
-# df_portmis['호출부호'].to_csv('C:/Users/user/Desktop/항만운영정보시스템data/ModelingDataset/호출부호.csv', encoding = 'cp949')
+df_portmis = df_portmis.drop(['입항횟수', '입항횟수.1'], axis = 1)
+
+
+# In[547]:
+
+
+df_portmis
+
+
+# In[548]:
+
+
+df_portmis['호출부호'].to_csv('C:/Users/user/Desktop/항만운영정보시스템data/ModelingDataset/호출부호.csv', encoding = 'cp949')
 
 
 # ### mmsi 정보 불러와서 df_ mmsi 만들기
 # + 01.20 교수님 확인 위해 수작업. 차주 크롤링 예정
 
-# In[450]:
+# In[549]:
 
 
-df_mmsi = pd.read_csv("C:/Users/user/Desktop/항만운영정보시스템data/ModelingDataset/호출부호_sample.csv", encoding = 'cp949')
+df_mmsi = pd.read_csv("C:/Users/user/Desktop/항만운영정보시스템data/ModelingDataset/(완)호출부호_mmsi매칭.csv", encoding = 'cp949', index_col=0)
 
 
-# In[451]:
+# In[550]:
 
 
-df_mmsi = df_mmsi.dropna()
+df_mmsi.isnull().sum()
 
 
-# In[452]:
+# In[551]:
 
 
-df_mmsi.iloc[:,2]
+df_mmsi
 
 
 # ### df_mmsi, df_portmis 합치기 => df_ais
 
-# In[454]:
+# In[552]:
 
 
-df_portmis.insert(2, 'mmsi', df_mmsi.iloc[:,2])
+# df_portmis의 3번째 열에 df_mmsi 추가 
+df_portmis.insert(2, 'mmsi', df_mmsi['mmsi'])
 
 
-# In[455]:
+# In[553]:
 
 
 df_portmis
 
 
-# In[456]:
+# In[554]:
 
 
-df_portmis_sample = df_portmis.dropna()
+df_portmis.info()
 
 
-# In[458]:
+# In[555]:
 
 
-df_portmis_sample.reset_index(drop=True)
+df_portmis
+
+
+# ### df_portmis = df_mmsi + df_portmis // Null값 제거
+
+# In[556]:
+
+
+df_portmis.isnull().sum()
+
+
+# In[559]:
+
+
+df_portmis.dropna().reset_index(drop=True)
+
+
+# ### port-mis 입출항정보 csv저장
+
+# In[560]:
+
+
+df_portmis_sample.to_csv("C:/Users/user/Desktop/항만운영정보시스템data/ModelingDataset/portmis.csv", sep = ',',encoding = 'cp949')
 
